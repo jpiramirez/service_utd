29d22e2964b860c98046a50da9c4dcb79afa6cd8
Juan-Pablo Ramirez <pablo.ramirez@utdallas.edu>
2016-02-17 17:41:00 -0600

    Separated the Gazebo sim launcher from the MC trials to avoid bottlenecks.

--- scripts/montecarlo.sh
+++ scripts/montecarlo.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 for i in `seq 1 $1`
   do
+    # Bringing down the controllers to avoid having windup of the inputs
     rosservice call /uav1/controller_manager/switch_controller "{start_controllers: [], stop_controllers: ['controller/twist'], strictness: 2}"
     rosservice call /uav2/controller_manager/switch_controller "{start_controllers: [], stop_controllers: ['controller/twist'], strictness: 2}"
     rosservice call /uav3/controller_manager/switch_controller "{start_controllers: [], stop_controllers: ['controller/twist'], strictness: 2}"
@@ -9,15 +10,21 @@ for i in `seq 1 $1`
     rosservice call /uav2/controller_manager/unload_controller "name: 'controller/twist'"
     rosservice call /uav3/controller_manager/unload_controller "name: 'controller/twist'"
     rosservice call /uav4/controller_manager/unload_controller "name: 'controller/twist'"
+
+    # Returning the robots to their original positions
+    rosservice call /gazebo/reset_world
+
+    # Bringing up the controllers again
     rosservice call /uav1/controller_manager/load_controller "name: 'controller/twist'"
     rosservice call /uav2/controller_manager/load_controller "name: 'controller/twist'"
     rosservice call /uav3/controller_manager/load_controller "name: 'controller/twist'"
     rosservice call /uav4/controller_manager/load_controller "name: 'controller/twist'"
-    rosservice call /gazebo/reset_world
+
     POSITION=`rosrun service_utd randvertex.py`
+    # Positioning the ground robot at a random road intersection
     rosservice call /gazebo/set_model_state '{model_state: { model_name: p3dx, pose: { position: '"$POSITION"', orientation: {x: 0, y: 0, z: 0, w: 0 } }, twist: { linear: {x: 0.0 , y: 0 ,z: 0 } , angular: { x: 0.0 , y: 0 , z: 0.0 } } , reference_frame: world } }'
     roslaunch service_utd urban_search_sim_multiuav_batch.launch
-    sleep 3
+    sleep 5
 
 
   done
